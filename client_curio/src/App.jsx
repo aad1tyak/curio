452507d1b32b2544a47912d0f65df14ea6193771
Aaditya <unredaaditya36@gmail.com>
2026-06-02 12:28:10 -0400

    style: update homepage layout and logo color

--- client_curio/src/App.jsx
+++ client_curio/src/App.jsx
@@ -52,15 +52,47 @@ const HomePage = ({ onSearch }) => {
   };
 
   return (
-    <div className="">
-      <form onSubmit={submitQuery}>
-        <input
-          type="text"
-          placeholder="Search..."
-          value={query}
-          onChange={handleChange}
+    <div className="bg-[#f6f4f0] flex flex-col items-center justify-center min-h-screen bg-neutral-primary px-4">
+      <div className="mb-8 h-24 overflow-hidden flex items-center justify-center select-none">
+        <img
+          src="../assests/curio_logo.svg"
+          className="h-full w-auto object-contain scale-100 -mt-px -ml-px"
         />
-        <button type="submit">Search</button>
+      </div>
+      <form onSubmit={submitQuery} className="w-full max-w-md mx-auto">
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeWidth="2"
+                d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="text"
+            className="block w-full p-3 ps-9 bg-white border-1 broder-gray-100 text-black text-sm rounded-none focus:outline-none focus:border-[#f0f0f0] focus:ring-1 focus:ring-gray-500 placeholder:text-gray-400 font-mono"
+            placeholder="Search..."
+            value={query}
+            onChange={handleChange}
+          />
+          <button
+            type="submit"
+            className="absolute end-2 top-1/2 -translate-y-1/2 text-black bg-[#f0f0f0] hover:bg-gray-300 active:bg-gray-300 font-mono text-xs px-4 py-1.5 rounded-none font-semibold tracking-wide uppercase transition-colors"
+          >
+            Search
+          </button>
+        </div>
       </form>
       <div className="" onClick={handleSurprize}>
         Surprize Me?
